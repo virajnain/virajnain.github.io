6bbb35766248a229fc4bf8296a3edf774954147f
Viraj Nain <virajnain174@gmail.com>
2025-08-16 14:33:21 -0500

    changed layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
-import Header from "@/components/header";
-import './globals.css'
+import '@/app/globals.css'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -16,13 +15,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 h-[5000px]` }>
+      <body className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 overflow-x-hidden` }>
 
-        <div className="bg-[#fbe2e3] absolute top-[-1rem] -z-10 left-[40%] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
-        <div className="bg-[#e2e3fb] absolute top-[-1rem] -z-10 right-[40%] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
-        <div className="bg-[#e3fbe2] absolute top-[15rem] -z-10 left-1/2 -translate-x-1/2 right-[0%] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
-
-        <Header />
+        <div className="bg-[#fbe2e3] absolute top-[-1rem] -z-10 left-[40%] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[50.75rem]"></div>
+        <div className="bg-[#e2e3fb] absolute top-[-1rem] -z-10 right-[40%] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[50.75rem]"></div>
+        <div className="bg-[#e3fbe2] absolute top-[15rem] -z-10 left-1/2 -translate-x-1/2 right-[0%] h-[31.25rem] w-[30rem] rounded-full blur-[10rem] sm:w-[50.75rem]"></div>
+      
         {children}
       </body>
     </html>
