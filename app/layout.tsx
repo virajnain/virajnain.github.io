c4e9845e115619ff984590a41cab5dd8d6331397
Viraj Nain <virajnain174@gmail.com>
2025-08-17 17:52:00 -0500

    fixing layout for mobile

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 overflow-x-hidden`}>
+      <body className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36`}>
         <div className="fixed inset-0 -z-10 pointer-events-none">
           <div className="bg-[#f5b5b8] absolute top-[-1rem] left-[40%] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[50.75rem]" />
           <div className="bg-[#b5b8f5] absolute top-[-1rem] right-[40%] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[50.75rem]" />
